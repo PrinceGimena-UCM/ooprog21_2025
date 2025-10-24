2856d94ecdf8bfa3a547eae2c166d44ba3eb9c93
PrinceGimena-UCM <gimenaprince2@gmail.com>
2025-10-24 17:55:15 +0800

    Add files via upload

---
+++ Chapter_6/BankBalance2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BankBalance2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double balance = 100.00;
+        double interestRate = 0.03;
+        int year = 1;
+        int userChoice;
+
+        do {
+            balance = balance + (balance * interestRate);
+            System.out.printf("After year %d at %.2f interest rate, balance is $%.4f%n",
+                              year, interestRate, balance);
+            System.out.println();
+            System.out.print("Do you want to see the balance at the end of another year?\n" +
+                             "Enter 1 for yes\n" +
+                             "or any other number for no >> ");
+            userChoice = input.nextInt();
+            year++;
+        } while (userChoice == 1);
+
+        System.out.println("----jGRASP: Operation complete.");
+    }
+}
