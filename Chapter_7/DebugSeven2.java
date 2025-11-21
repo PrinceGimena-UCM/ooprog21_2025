c99ed325691846793e4c64a2b055c4a67e4f50fe
PrinceGimena-UCM <gimenaprince2@gmail.com>
2025-11-21 08:26:18 +0800

    Add files via upload

--- Chapter_7/DebugSeven2.java
+++ Chapter_7/DebugSeven2.java
@@ -4,36 +4,36 @@
 import java.util.*;
 public class DebugSeven2
 {
-    public static void main(String[] args)
-    {
-        String str;
-        int x;
-        int length;
-        int start = 0;
-        int num;
-        int lastSpace = -1;
-        int sum = 0;
-        String partStr;
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter a series of integers separated by spaces >> ");
-        str = in.nextLine();
-        length = str.length();
-        for(x = 0; x < length; ++x)
-        {
-            if(str.charAt(x) == ' ')
-            {
-                partStr = str.substring(lastSpace + 1, x);
-                num = Integer.parseInt(partStr);
-                System.out.println(" " + num);
-                sum += num;
-                lastSpace = x;
-            }
-        }
-        partStr = str.substring(lastSpace + 1, length);
-        num = Integer.parseInt(partStr);
-        System.out.println(" " + num);
-        sum += num;
-        System.out.println(" -------------------" +
-            "\nThe sum of the integers is " + sum);
-    }
+   public static void main(String[] args)
+   {
+      String str;
+      int x;
+      int length;
+      int start = 0;
+      int num;
+      int lastSpace = -1;
+      int sum = 0;
+      String partStr;
+      Scanner in = new Scanner(System.in);
+      System.out.print("Enter a series of integers separated by spaces >> ");
+      str = in.nextLine();
+      length = str.length(); // i changed len to length
+      for(x = 0; x < length; ++x)  // i changed len to length
+      {
+         if(str.charAt(x) == ' ')
+         {
+            partStr = str.substring(lastSpace + 1, x);
+            num = Integer.parseInt(partStr);
+            System.out.println(" " + num);
+            sum += num;
+            lastSpace = x;
+         }
+      }
+      partStr = str.substring(lastSpace + 1, length);
+      num = Integer.parseInt(partStr);  // i changed parStr to partStr
+      System.out.println(" " + num);
+      sum += num;  // add +
+      System.out.println(" -------------------" +
+         "\nThe sum of the integers is " + sum);
+   }
 }
